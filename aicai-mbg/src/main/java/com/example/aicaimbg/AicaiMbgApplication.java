55fa31243268d95332403b86c476115d18b4f461
niuhao <heiENs5RNZ4nK0Qxe>
2024-12-09 20:31:06 +0800

    新建生成代码模块

---
+++ aicai-mbg/src/main/java/com/example/aicaimbg/AicaiMbgApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicaimbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicaiMbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicaiMbgApplication.class, args);
+    }
+
+}
---
+++ aicai-mbg/src/test/java/com/example/aicaimbg/AicaiMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicaimbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicaiMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
