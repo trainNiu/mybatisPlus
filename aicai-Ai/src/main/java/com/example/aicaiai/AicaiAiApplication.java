418a50c82df3fa07d0f8f6539ab1a7ee7d4eb093
niuhao <heiENs5RNZ4nK0Qxe>
2025-02-19 18:37:57 +0800

    AI

---
+++ aicai-Ai/src/main/java/com/example/aicaiai/AicaiAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicaiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicaiAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicaiAiApplication.class, args);
+    }
+
+}
---
+++ aicai-Ai/src/test/java/com/example/aicaiai/AicaiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aicaiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicaiAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
